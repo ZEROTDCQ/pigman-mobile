64fe8def4481a55e1e5b7daf3b58a73c859c693f
fang <1448293023@qq.com>
2019-12-17 21:58:11 +0800

    各商品详情页

---
+++ src/views/Detail/js/flash/detailFlashing.js
@@ -0,0 +1,11 @@
+// 限时抢购-正在疯抢
+
+
+
+let makeSwiper = new Swiper('.swiper-container-make', {
+  slidesPerView: 'auto',
+});
+
+let recommSwiper = new Swiper('.swiper-container-recommend', {
+  slidesPerView: 'auto',
+});
---
+++ src/views/Home/components/HomeNewtaste/HomeNewtaste.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- 新品尝鲜 -->
+  <div class="area-box">
+    <div class="area-head clearfix">
+      <h3 class="ah-title">{{data.title}}</h3>
+      <div class="ah-sub-title">{{data.sketch}}</div>
+      <p class="btn-enter-area">
+        <a href="/sc/flash">
+          进入专区
+          <span>&gt;</span>
+        </a>
+      </p>
+    </div>
+    <div class="area-body pro-list">
+      <NewtasteItem v-for="(item,index) in data.son" :key="index" :data="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import NewtasteItem from "@/components/ProCard/Small/Newtaste/NewtasteItem";
+export default {
+  components: {
+    NewtasteItem
+  },
+  props: {
+    data: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+/deep/.pro-list {
+  margin-top: -10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .pro-item {
+    margin-top: 10px;
+  }
+}
+</style>
